f90e289e65af111a71ea6108dc09c87b66f359a3
Skarline <40340562+skarline@users.noreply.github.com>
2020-04-26 17:09:07 -0300

    Update upload_images.php

--- upload_images.php
+++ upload_images.php
@@ -15,12 +15,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     for ($i = 0; $i < $filesAmount; $i++) {
       // Tomo la extensión del archivo ('png', 'jpg', 'jpeg', etc.)
       $fileName = $_FILES['files']['name'][$i];
-      $fileExtention = pathinfo($_FILES['files']['name'][$i], PATHINFO_EXTENSION);
       
       /* Creo la ruta donde se guardará el nuevo archivo:
        * carpeta + número de archivo + extensión del archivo
        */
-      $targetPath = "{$path}/{$fileName}.{$fileExtention}";
+      $targetPath = "{$path}/{$fileName}";
 
       // Muevo el archivo a la ruta en el servidor
       move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetPath);
